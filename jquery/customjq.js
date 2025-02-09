0057362575439482910cd9b4dbaa26064e7a6b15
aidokong <theictuniversityofnigeria@gmail.com>
2025-02-09 11:44:57 +0100

    Add files via upload

--- jquery/customjq.js
+++ jquery/customjq.js
@@ -22,5 +22,40 @@ $(document).ready(function() {
   });
 
 
+   $('.read-more-btn').on('click', function(event) {
+            event.preventDefault();
+            var $this = $(this);
+            var $additionalContent = $this.closest('.news__data').find('.news__additional');
+            $additionalContent.slideToggle(function() {
+                $this.text($additionalContent.is(':visible') ? 'Read Less' : 'Read More');
+            });
+        });
+
+
+        $('.read-more-btn1').on('click', function(event) {
+          event.preventDefault();
+          var $this = $(this);
+          var $additionalContent = $this.closest('.news__data1').find('.news__additional1');
+          $additionalContent.slideToggle(function() {
+              $this.text($additionalContent.is(':visible') ? 'Read Less' : 'Read More');
+          });
+      });
+
+        $('.read-more-btn2').on('click', function(event) {
+          event.preventDefault();
+          var $this = $(this);
+          var $additionalContent = $this.closest('.news__data2').find('.news__additional2');
+          $additionalContent.slideToggle(function() {
+              $this.text($additionalContent.is(':visible') ? 'Read Less' : 'Read More');
+          });
+      });
+        $('.read-more-btn3').on('click', function(event) {
+          event.preventDefault();
+          var $this = $(this);
+          var $additionalContent = $this.closest('.news__data3').find('.news__additional3');
+          $additionalContent.slideToggle(function() {
+              $this.text($additionalContent.is(':visible') ? 'Read Less' : 'Read More');
+          });
+      });
 
 }); 
